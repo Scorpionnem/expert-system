e695deea1bb34a51d02527c5ac7da9f715ec3795
Mewen Batty <scorpionem86@gmail.com>
2025-12-10 14:27:40 +0100

    proof of concept

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 13:26:29 by mbatty            #+#    #+#             */
-/*   Updated: 2025/12/10 13:46:18 by mbatty           ###   ########.fr       */
+/*   Updated: 2025/12/10 14:27:30 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 /*
 	RULES:
@@ -37,8 +39,121 @@
 	Quand on veux verifier une query (?D), on envoie la liste de facts que l'on connait a notre expert system, il passe dans toutes les rules et si a la fin D est true, on valide la query
 */
 
+struct	Fact
+{
+	Fact(char c, bool state)
+	{
+		this->c = c;
+		this->state = state;
+	}
+	char	c;
+	bool	state = false;
+};
+
+enum class	ConditionType
+{
+	AND,
+	OR,
+	EXISTS, // Returns if condition's fact is true
+};
+
+class	ComputingError: public std::exception
+{
+	public:
+		ComputingError(): _msg("Computing error") {}
+		ComputingError(const std::string &msg): ComputingError()
+		{
+			_msg += " ";
+			_msg += msg;
+		}
+		virtual const char*	what() const throw()
+		{
+			return (_msg.c_str());
+		}
+	private:
+		std::string	_msg;
+};
+
+struct	Condition
+{
+	struct Condition	*left;
+	struct Condition	*right;
+	Fact				*fact;
+	ConditionType		type;
+
+	Condition(ConditionType type, struct Condition *left, struct Condition *right, Fact *fact)
+	{
+		this->type = type;
+		this->left = left;
+		this->right = right;
+		this->fact = fact;
+	}
+	bool	compute()
+	{
+		if (type == ConditionType::EXISTS)
+			return (fact->state);
+		if (type == ConditionType::AND)
+			return (left->compute() && right->compute());
+		if (type == ConditionType::OR)
+			return (left->compute() || right->compute());
+		throw ComputingError("ConditionType not found");
+	}
+};
+
+struct	Rule
+{
+	Rule(Condition *condition)
+	{
+		this->condition = condition;
+	}
+	Condition	*condition;
+	bool	compute()
+	{
+		return (condition->compute());
+	}
+};
+
 int	main(void)
 {
+	{
+		// (A | B) & C => D
+		Fact	A('A', true);
+		Fact	B('B', false);
+		Fact	C('C', true);
+	
+		Condition	A_EXISTS(ConditionType::EXISTS, NULL, NULL, &A);
+		Condition	B_EXISTS(ConditionType::EXISTS, NULL, NULL, &B);
+		Condition	C_EXISTS(ConditionType::EXISTS, NULL, NULL, &C);
+	
+		Condition	A_OR_B(ConditionType::OR, &A_EXISTS, &B_EXISTS, NULL);
+		Condition	A_OR_B_AND_C(ConditionType::AND, &A_OR_B, &C_EXISTS, NULL);
+	
+		Rule	rule(&A_OR_B_AND_C);
+	
+		Fact	D('D', rule.compute());
+		std::cout << "(A | B) & C => D is: " << D.state << std::endl;
+	}
+
+	{
+		// A & B & C => D -> transforme en: (A & B) & C
+		// Il faut transformer avec des parentheses car fonctionne que par bloc de 2
+		// (Pas sur que ce soit valide sur le long terme mais c'est la solution a la quelle j'ai pense sur le moment)
+		Fact	A('A', true);
+		Fact	B('B', true);
+		Fact	C('C', true);
+
+		Condition	A_EXISTS(ConditionType::EXISTS, NULL, NULL, &A);
+		Condition	B_EXISTS(ConditionType::EXISTS, NULL, NULL, &B);
+		Condition	C_EXISTS(ConditionType::EXISTS, NULL, NULL, &C);
+
+		Condition	A_AND_B(ConditionType::AND, &A_EXISTS, &B_EXISTS, NULL);
+		Condition	A_AND_B_AND_C(ConditionType::AND, &A_AND_B, &C_EXISTS, NULL);
+
+		Rule	rule(&A_AND_B_AND_C);
+	
+		Fact	D('D', rule.compute());
+		std::cout << "A & B & C => D is: " << D.state << std::endl;
+	}
 	std::cout << "expert-system" << std::endl;
 	return (0);
 }
